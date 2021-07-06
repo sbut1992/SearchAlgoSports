db0d64fa37f4e566a2b1d152c31787b5074b3cd1
Mathïs Fédérico <mathfederico@gmail.com>
2021-07-06 19:47:42 +0200

    Added wandb integration Moved run.py to root

--- run.py
+++ run.py
@@ -2,16 +2,23 @@ from src.processing.load_dataframe import load_dataframe
 from src.search.mcts.mcts import MCTSTree
 import time
 import wandb
-wandb.init(project='searchalgosportsteam', entity='searchalgosports')
-config = wandb.config
 
 if __name__ == '__main__':
+
+    hyperparameters_default = {
+        'exploration': 5.
+    }
+
+    wandb.init(project='searchalgosportsteam', entity='searchalgosports',
+        config=hyperparameters_default)
+    config = wandb.config
+
     empty_positions = ['PG','SG','SF','PF','C','F','G','UTIL']
     dataframe = load_dataframe('2021_03_12_slate.csv', empty_positions)
     budget = 50000
-    mcts_tree = MCTSTree(dataframe, empty_positions, budget, exploration=5.)
+    mcts_tree = MCTSTree(dataframe, empty_positions, budget, exploration=config.exploration)
 
     start = time.time()
-    best_node = mcts_tree.run(500000)
+    best_node = mcts_tree.run(5000)
     print(f'Best config found in {time.time()-start:.1f}s: {best_node} with value {best_node.value}')
 
